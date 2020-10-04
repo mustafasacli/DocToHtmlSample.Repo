5af585af1b414ca5d9c069e7d64a30108e71b724
Mustafa.SACLI <Mustafa.SACLI@GSBBIDB330>
2020-10-04 22:53:12 +0300

    first commit.

---
+++ DocToHtmlSampleConsoleApp/DocToHtmlSampleConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace DocToHtmlSampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertDocToHtml(@"C:\Users\mustafa.sacli\Desktop\Ilt__Tablolar_Ayri\EK-10.docx", 
+                @"C:\Users\mustafa.sacli\Desktop\Ilt__Tablolar_Ayri\EK-10.html");
+            Console.ReadKey();
+        }
+        public static void ConvertDocToHtml(object Sourcepath, object TargetPath)
+        {
+
+            Word._Application newApp = new Word.Application();
+            Word.Documents d = newApp.Documents;
+            object Unknown = Type.Missing;
+            Word.Document od = d.Open(ref Sourcepath, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown,
+                                     ref Unknown, ref Unknown, ref Unknown, ref Unknown);
+            object format = Word.WdSaveFormat.wdFormatFilteredHTML;
+
+
+
+            newApp.ActiveDocument.SaveAs(ref TargetPath, ref format,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown, ref Unknown,
+                        ref Unknown, ref Unknown);
+
+            newApp.Documents.Close(Word.WdSaveOptions.wdDoNotSaveChanges);
+            newApp.Quit();
+
+        }
+    }
+}
